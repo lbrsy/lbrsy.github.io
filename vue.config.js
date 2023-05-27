4b8a2a735b0b3c905e51ab37423461abe96a0ae5
lbrsy <51570466+lbrsy@users.noreply.github.com>
2023-05-27 12:46:11 +0800

    标题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:"docs"
+  outputDir:"docs",
+  chainWebpack: config =>{
+  config.plugin('html')
+    .tap(args => {
+      args[0].title = "备忘录";
+      return args;
+    })
+  }
 })
+
